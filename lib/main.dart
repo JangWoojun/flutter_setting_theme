14e37baf139f0e30f9f41fa3067a983bee3cd4b7
JangWoojun <woojun0107@naver.com>
2024-01-23 16:30:15 +0900

    english_words 사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Body(),
+    ),
+  ));
 }
 
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final list = generateWordPairs().take(5).toList();
+    final wordTexts = list
+        .map((word) => Text(
+              word.asString,
+              style: const TextStyle(fontSize: 22, color: Colors.blue),
+            ))
+        .toList();
+    return SafeArea(
+        child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: wordTexts,
+            )));
+  }
+}
