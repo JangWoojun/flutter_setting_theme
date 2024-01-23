a217ead7e229c31765b9749d4c03e4c4202f0348
JangWoojun <woojun0107@naver.com>
2024-01-23 18:19:33 +0900

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,44 @@ import 'package:flutter/material.dart';
 const imagePath = "assets/images/";
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
+    theme: customTheme,
     home: Scaffold(
-      body: Body(),
+      body: const Body(),
+      appBar: AppBar(title: const Text("app bar"),),
+      floatingActionButton: FloatingActionButton(child: const Icon(Icons.bug_report), onPressed: () => print("fab")),
     ),
   ));
 }
+
+final customTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+  // const ColorScheme.light(
+  //   primary: Colors.indigo,
+  //   secondary: Colors.green,
+  //   tertiary: Colors.yellow
+  // ),
+  appBarTheme: const AppBarTheme(backgroundColor: Colors.blue),
+  useMaterial3: true,
+  textTheme: const TextTheme(
+      bodyMedium: TextStyle(fontWeight: FontWeight.bold, fontSize: 130)
+  ),
+);
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final textThem = customTheme.textTheme;
 
     return SafeArea(
-        child: SingleChildScrollView(child: Center(child: Image.asset("$imagePath/image.jpg"),))
+        child: SingleChildScrollView(child: Center(child: Column(
+          children: [
+            Text("aaa", style: textThem.bodyMedium,),
+            Image.asset("$imagePath/image.jpg"),
+          ],
+        ),))
     );
   }
 }
